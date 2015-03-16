7aa1be35a7b842be7f7c426fe9f258c2b4dccaa9
Tim Jarratt <tjarratt@gmail.com>
2015-03-15 22:26:21 -0700

    Initial implementation of pyramid
    Doesn't work because lein doesn't seem to want to allow me to
    require penumbra.

--- serpinski/src/serpinski/core.clj
+++ serpinski/src/serpinski/core.clj
@@ -1,7 +1,27 @@
 (ns serpinski.core
   (:gen-class))
 
+;; Serpinski helpers ahoy
+(defn draw-pyramid []
+  (material 0.8 0.2 0.2 1)
+
+  ;; draw the four sides of the pyramid
+  (draw-triangle-fan
+   (vertex 0 1 0)
+   (dotimes [_ 5]
+            (rotate 90 0 1 0)
+            (normal 1 0.5 1)
+            (vertex 0.5 0 0.5)))
+
+  ;; draw the base of the pyramid
+  (draw-quads
+   (normal 0 -1 0)
+   (dotimes [_ 4]
+            (rotate -90 0 1 0)
+            (vertex 0.5 0 0.5))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (draw-pyramid))
